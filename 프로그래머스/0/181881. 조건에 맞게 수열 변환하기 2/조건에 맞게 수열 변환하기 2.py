2fc74641ea515a9f393a98ee80cefe4e2570336f
SeoHyeon Jeong <67922080+jsh1021902@users.noreply.github.com>
2024-05-20 23:20:20 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -0,0 +1,20 @@
+def solution(arr):
+    index = 0
+    
+    while True:
+        change = []
+        for i in arr:
+            if i >= 50 and i % 2 == 0:
+                change.append(int(i / 2))
+            elif i < 50 and i % 2 == 1:
+                change.append(i * 2 + 1)
+            else:
+                change.append(i)
+        
+        same = all(a == b for a, b in zip(arr, change))
+        if same:
+                break
+
+        index += 1
+        arr = change
+    return index
