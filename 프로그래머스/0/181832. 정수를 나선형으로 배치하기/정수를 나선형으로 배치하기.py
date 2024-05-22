fd7f8c876b443accc11d3b6eb77ad9037344d95e
SeoHyeon Jeong <67922080+jsh1021902@users.noreply.github.com>
2024-05-22 19:19:23 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.43 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = [[0 for j in range(n)] for i in range(n)]
+    move = [[0,1], [1,0], [0,-1], [-1,0]]
+    x, y, m = 0, 0, 0
+    for i in range(1, n**2 + 1):
+        answer[y][x] = i
+        if y + move[m][0] >= n or x + move[m][1] >= n or answer[y + move[m][0]][x + move[m][1]]:
+            m = (m+1) % len(move)
+        y, x = y + move[m][0], x + move[m][1]
+    return answer
